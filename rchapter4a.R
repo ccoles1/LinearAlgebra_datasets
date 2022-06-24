4649e0fb3e6ad77d3310e1f6da79526de833c3f9
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-24 11:23:02 -0400

    Update rchapter4a.R

--- rchapter4a.R
+++ rchapter4a.R
@@ -1,8 +1,8 @@
-require(matrixcalc)#install this package
-require(matlib)#install this package
-library(matrixcalc)
-library(mpoly)
-require(mpoly)#install this package
+require('matrixcalc')#install this package
+require('matlib')#install this package
+library('matrixcalc')
+library('mpoly')
+require('mpoly')#install this package
 M <- matrix(c(20,9,1,5,20,8,19,20,19,20),nrow = 2, ncol = 5, byrow = TRUE)
 A <- matrix(c(1,2,3,4),nrow = 2, ncol = 2, byrow = TRUE)
 print(A)
