82d36bf4c560b1b0c3ebc18bdeffceedae6c1d2b
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-27 19:31:40 -0400

    Update rchapter5a.R

--- rchapter5a.R
+++ rchapter5a.R
@@ -2,3 +2,20 @@ install.packages('matrixcalc')
 library('matrixcalc')
 data<- created csv file
 subscriptlist=matrix(c(0,1,2,3,4,20,21,22,23,24,209,210,214,213,215))
+
+# pick a sample to plot
+w<-matrix(0, 15)
+for (i in 1:15){
+    w[i]=1/15
+}
+train<-matrix(0,length(subscriptlist),64])
+for (j in 1:length(subscriptlist)){
+        train[j]=X[subscriptlist[j]]
+ }
+train<-t(train)
+
+def phi(x):
+#sigmoid activation function
+    return 1/(1+math.exp(-x))
+# hyperbolic tan activation function
+#    return np.tanh(x)
