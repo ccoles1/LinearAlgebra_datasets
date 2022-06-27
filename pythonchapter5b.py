19d460cd207516194d38ecafc5db91297a335ab7
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-27 15:24:49 -0400

    Update pythonchapter5b.py

--- pythonchapter5b.py
+++ pythonchapter5b.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-#y=np.array([607,576,638,615,655,536,358,539,432,460,445,428,448,367,470,401])
-#x=np.array(range(0,len(y)))
-y=np.array([607,576,638,61,655])
-x=np.array(range(0,len(y)))
+cases=np.array([607,576,638,615,655,536,358,539,432,460,445,428,448,367,470,401])
+x=np.array(range(1,len(cases)+1))
+y=np.zeros(len(x))
+for i in range(0,len(y)):
+    y[i]=cases[0]
+    for j in range(0,i):
+        y[i]=y[i]+cases[j]
 a0=np.array([5.5,.4])
-steps=3
+steps=4
 for i in range(0,steps):
-    #plotting the model
-    plt.scatter(x,y)
-    plt.plot(x,np.exp(a0[0]+a0[1]*x))
+#plotting the model
+    xsmall=np.linspace(1,5,10)
+    plt.scatter(x[0:5],y[0:5])
+    plt.plot(xsmall,np.exp(a0[0]+a0[1]*xsmall),label=i)
     A=np.zeros([len(y),2])
     W=np.zeros([len(y),len(y)])
     for i in range(0,len(y)):
@@ -20,4 +24,5 @@ for i in range(0,steps):
     gradients=np.transpose(A).dot(y)-np.transpose(A).dot(np.exp(A.dot(a0)))
     H=np.transpose(A).dot(W).dot(A)
     a0=a0-np.linalg.inv(H).dot(gradients)
+plt.legend()
 plt.show()
