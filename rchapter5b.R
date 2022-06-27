800f0197bfbd3313e98a0884f718dfcbc648aad9
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-27 17:30:13 -0400

    Update rchapter5b.R

--- rchapter5b.R
+++ rchapter5b.R
@@ -9,3 +9,24 @@ for (i in 1:length(y)){
     }
 }
 a0<-matrix(c(5.5,.4))
+A<-matrix(0,length(y),2)
+W<-matrix(0,length(y),length(y))
+for (i in 1:length(y)){
+    A[i,1]<-1
+    A[i,2]<-x[i]
+    W[i,i]<-exp(a0[1]+a0[2]*x[i])
+}
+gradients<-t(A)%*%y-t(A)%*%(exp(A%*%a0))
+H<-t(A)%*%W%*%A
+a0<-a0-inv(H)%*%gradients
+
+#plotting the model
+xsmall <- seq(1, 5, by=0.1)
+plots<-matrix(0,length(xsmall),4)
+plot(x,y)
+data<-matrix(c(exp(a0[1]+a0[2]*xsmall)))
+for (i in 1:length(data)){
+    plots[i,1]<-data[i]
+}
+plot(xsmall,exp(a0[1]+a0[2]*xsmall),xlab = "Time",
+   ylab = "Cumulative Covid Cases",typ="l",col="green")
