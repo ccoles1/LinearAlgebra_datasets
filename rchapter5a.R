b1915a9faf4294a42ead61170a6be876f873e704
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-28 10:35:36 -0400

    Update rchapter5a.R

--- rchapter5a.R
+++ rchapter5a.R
@@ -42,3 +42,42 @@ cross_entropy<-function(actual, predicted){
     loss<-sum(actual*log(predicted))
     return(loss)
 }
+
+phimatrix<-matrix(0,63)
+phiprimematrix<-matrix(0,63)
+diff<-matrix(0,63)
+yhat<-matrix(0,63)
+yhat<-train%*%w
+check<-train%*%w
+partials<-matrix(0,length(subscriptlist)+1)
+softpartial<-matrix(0,length(subscriptlist)+1)
+
+r<-.03
+data<-matrix(0,63)
+data=X[226,]
+actual<-matrix(0,63)
+for (k in 1:63){
+    actual[k]<-1
+}
+for (steps in 0:1){
+        for (i in 1:63){
+            phimatrix[i,]<-phi((train%*%w)[i,])
+            phiprimematrix[i,]=phiprime((train%*%w)[i,])}
+        for (i in 1:63){
+            diff[i,]<-(X[i,]-phimatrix[i,])[1]}
+#        diff=data-softmax((train%*%w))
+#    partial derivatives when cost function is cross-entropy, cpartials
+#        partials=t(train)%*%(softmax(train%*%w)-data)
+        partials<-t(train)%*%(phiprimematrix*diff)
+#        cpartials=partials
+        for (i in 1:15){
+            w[i]<-w[i] -(2*r * partials)[i]
+#             w[i]<-w[i] -(r * cpartials)[i]
+            }
+# if you want w to be a percentage weight vector, let w=w/sum(w)
+        w<-w/sum(w)
+        yhat<-softmax(train%*%w)
+       SSE<-t(phimatrix-data)%*%(phimatrix-data)
+#        cerror<-cross_entropy(actual, yhat)
+        print(w)
+}
