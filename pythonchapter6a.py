3b6dcb16499f3650a4cdd345776c617e7f765516
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-29 12:23:07 -0400

    Update pythonchapter6a.py

--- pythonchapter6a.py
+++ pythonchapter6a.py
@@ -1,10 +1,5 @@
 data<-read.csv(file="stockreturn.csv",sep = ",")
 mat<-as.matrix(data)
-t1<-matrix(0,nrow(mat)-2)
-for(i in 1:nrow(X)){
-  for(j in 1:ncol(X)){
-    t1[i,j]<-as.numeric(mat[i+1,j+2])
-  }}
 
 returnAAL<-list()
 returndAL<-list()
@@ -12,16 +7,25 @@ returnUAL <-list()
 returnABBV<-list()
 returnCBG <-list()
 returnEQR<-list()
-for i in range (0,600000):
-    if t1[i, 6] == "AAL":
-        returnAAL<-np.append(returnAAL, t1[i, 4])
-    if t1[i, 6] == "ABBV":
-        returnABBV<-np.append(returnABBV, t1[i, 4])
-    if t1[i, 6] == "DAL":
-        returnDAL<-np.append(returnDAL, t1[i, 4])
-    if t1[i, 6] == "UAL":
-        returnUAL<-np.append(returnUAL, t1[i, 4])
-    if t1[i, 6] == "CBG":
-        returnCBG<-np.append(returnCBG, t1[i, 4])
-    if t1[i, 6] == "EQR":
-        returnEQR<-np.append(returnEQR, t1[i, 4])
+for (i in 2:600000){
+    if (mat[i, 7] == "AAL"){
+        returnAAL<-append(returnAAL, mat[i, 5])
+        }
+    if (mat[i, 7] == "ABBV"){
+        returnABBV<-append(returnABBV, mat[i, 5])
+        }
+    if (mat[i, 7] == "DAL"){
+        returndAL<-append(returndAL, mat[i, 5])
+        }
+    if (mat[i, 7] == "UAL"){
+        returnUAL<-append(returnUAL, mat[i, 5])
+        }
+    if (mat[i, 7] == "CBG"){
+        returnCBG<-append(returnCBG, t1[i, 5])
+        }
+    if (mat[i, 7] == "EQR"){
+        returnEQR<-append(returnEQR, mat[i, 5])
+        }
+    }
+x<-1:length(returnAAL)
+plot(x,returnAAL)
