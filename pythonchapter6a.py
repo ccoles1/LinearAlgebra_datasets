825f70068a27c15f17ab8ac947aac476cdc9660f
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-29 12:09:16 -0400

    Update pythonchapter6a.py

--- pythonchapter6a.py
+++ pythonchapter6a.py
@@ -1,1 +1,27 @@
+data<-read.csv(file="stockreturn.csv",sep = ",")
+mat<-as.matrix(data)
+t1<-matrix(0,nrow(mat)-2)
+for(i in 1:nrow(X)){
+  for(j in 1:ncol(X)){
+    t1[i,j]<-as.numeric(mat[i+1,j+2])
+  }}
 
+returnAAL<-list()
+returndAL<-list()
+returnUAL <-list()
+returnABBV<-list()
+returnCBG <-list()
+returnEQR<-list()
+for i in range (0,600000):
+    if t1[i, 6] == "AAL":
+        returnAAL<-np.append(returnAAL, t1[i, 4])
+    if t1[i, 6] == "ABBV":
+        returnABBV<-np.append(returnABBV, t1[i, 4])
+    if t1[i, 6] == "DAL":
+        returnDAL<-np.append(returnDAL, t1[i, 4])
+    if t1[i, 6] == "UAL":
+        returnUAL<-np.append(returnUAL, t1[i, 4])
+    if t1[i, 6] == "CBG":
+        returnCBG<-np.append(returnCBG, t1[i, 4])
+    if t1[i, 6] == "EQR":
+        returnEQR<-np.append(returnEQR, t1[i, 4])
