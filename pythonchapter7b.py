d6de58596070bb6b6be9d8166bcc67526232ceca
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-30 09:37:55 -0400

    Update pythonchapter7b.py

--- pythonchapter7b.py
+++ pythonchapter7b.py
@@ -1,12 +1,21 @@
+import pip
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
 #choose a training set, this will include 20 calls, at least 4 samples from sri lanka jungle fowl
 indices=[]
+n=10
 for i in range (0,16):
   indices=np.append(indices,random.randint(0, n))
-Temp = Table[0, {i, 1, 2}, {j, 1, 60}];
-elist = {};
-For[i = 1, i <= 100000 - 60,
-  Temp[[1]] = Take[s2list, {1 + i, 60 + i}];
-  Temp[[2]] = Take[s3list, {1 + i, 60 + i}];
+  
+# the data in birdcalls.zip are from audio calls, we will be looking at discrete fourier transforms of these calls.
+Temp = np.zeros([2,60])
+elist = []
+for i in range(0,100000 - 60):
+  Temp[0] = Take[s2list, {1 + i, 60 + i}];
+  Temp[1] = Take[s3list, {1 + i, 60 + i}];
   S = Temp . Transpose[Temp]/60;
-  elist = Append[elist, Max[N[Eigenvalues[S]]]];
-  i = i + 1];
+  w,v=np.linalg.eig(S)
+  elist = np.append(elist, np.max(v));
+
