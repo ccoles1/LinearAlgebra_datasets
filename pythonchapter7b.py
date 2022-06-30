5228a773d5cdbf18efabe03cd5e775475c4070c5
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-30 09:47:39 -0400

    Update pythonchapter7b.py

--- pythonchapter7b.py
+++ pythonchapter7b.py
@@ -3,13 +3,20 @@ import pandas as pd
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from scipy.fft import fft, ifft
+data = pd.read_csv('birdcalls1.csv')
+mat=np.array(data)
+
 #choose a training set, this will include 20 calls, at least 4 samples from sri lanka jungle fowl
 indices=[]
 n=10
 for i in range (0,16):
   indices=np.append(indices,random.randint(0, n))
+birdcall=mat[:,1]
   
 # the data in birdcalls.zip are from audio calls, we will be looking at discrete fourier transforms of these calls.
+f=np.abs(fft(birdcall))
+
 Temp = np.zeros([2,60])
 elist = []
 for i in range(0,100000 - 60):
