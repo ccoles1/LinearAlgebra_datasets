7458e1973c9316c0143250a0612421fa09d8ca36
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-06-30 09:55:27 -0400

    Update pythonchapter7b.py

--- pythonchapter7b.py
+++ pythonchapter7b.py
@@ -7,8 +7,16 @@ from scipy.fft import fft, ifft
 data = pd.read_csv('birdcalls1.csv')
 mat=np.array(data)
 
+#this function helps find indices of the largest peaks in the discrete fourier transform of the calls
+def sort_index(lst, rev=True):
+    index = range(len(lst))
+    s = sorted(index, reverse=rev, key=lambda i: lst[i])
+    return s
+  
 #choose a training set, this will include 20 calls, at least 4 samples from sri lanka jungle fowl
 indices=[]
+xpeaks=[]
+ypeaks=[]
 n=10
 for i in range (0,16):
   indices=np.append(indices,random.randint(0, n))
@@ -16,13 +24,6 @@ birdcall=mat[:,1]
   
 # the data in birdcalls.zip are from audio calls, we will be looking at discrete fourier transforms of these calls.
 f=np.abs(fft(birdcall))
-
-Temp = np.zeros([2,60])
-elist = []
-for i in range(0,100000 - 60):
-  Temp[0] = Take[s2list, {1 + i, 60 + i}];
-  Temp[1] = Take[s3list, {1 + i, 60 + i}];
-  S = Temp . Transpose[Temp]/60;
-  w,v=np.linalg.eig(S)
-  elist = np.append(elist, np.max(v));
+xpeaks=np.append(xpeaks,sort_index(f)[0])
+ypeaks=np.append(xpeaks,sort_index(f)[1])
 
