39552edfba4586251848500924f51a6698687563
ccoles1 <58393763+ccoles1@users.noreply.github.com>
2022-07-01 14:33:58 -0400

    Update rchapter7b.R

--- rchapter7b.R
+++ rchapter7b.R
@@ -2,6 +2,7 @@ install.packages('matrixcalc')
 library('matrixcalc')
 
 data<-read.csv(file="birdcalls1.csv",header = FALSE,sep = ",")
+#birdcalls.csv is located in birdcallsupdated.zip
 mat<-as.matrix(data)
 X<-matrix(0,nrow(mat)-2,ncol(mat)-2)
 for(i in 1:nrow(X)){
@@ -9,50 +10,52 @@ for(i in 1:nrow(X)){
     X[i,j]<-as.numeric(mat[i+1,j+2])
   }}
 
-decision<-list()
-
 sort_index<-function(x){
     s<-sort(x, index.return=TRUE)$ix
     return(s)
 }
 
+decision<-list()
+
 n<-10
 jfn<-4
 
-#Sri Lankan Jungle fowl bird calls are between col1 and col2
+# 2n is the number of non Sri Lankan Jungle fowl in the training set, jfn is the number of Sri Lankan Jungle fowl in the training set. This can be changed.
+#Sri Lankan Jungle fowl bird calls are between col1 and col2, in the dataset Sri Lankan Jungle fowl are in columns 17 to 31
+
 col1<-3
 col2<-7
-indices<-list()
-jfindices<-list()
-xpeaks<-list()
-ypeaks<-list()
+for (run in 1:5){
+    indices<-list()
+    jfindices<-list()
+    xpeaks<-list()
+    ypeaks<-list()
+    xconstraints<-list()
+    yconstraints<-list()
+    indices1<-sample(1:col1, n, replace=T)
+    indices2<-sample(col2:ncol(mat),n,replace=T)
+    indices<-c(indices1,indices2)
+    jfindices<-sample(col1:col2,jfn,replace=T)
 
-xconstraints<-list()
-yconstraints<-list()
-indices1<-sample(1:col1, n, replace=F)
-indices2<-sample(col2:ncol(mat),n,replace=F)
-indices<-c(indices1,indices2)
-jfindices=sample(col1:col2,jfn,replace=F)
+    for (i in 1:length(indices)){
+        birdcall<-mat[,indices[i]]
+        f<-abs(fft(birdcall))
+        xpeaks<-append(xpeaks,sort_index(f)[1]/1000)
+        ypeaks<-append(ypeaks,(sort_index(f)[2]-sort_index(f)[1])/sort_index(f)[1])
+    }
 
-for (i in 1:length(indices)){
-    birdcall<-mat[,indices[i]]
-    f<-abs(fft(birdcall))
-    xpeaks<-append(xpeaks,sort_index(f)[1]/1000)
-    ypeaks<-append(ypeaks,(sort_index(f)[2]-sort_index(f)[1])/sort_index(f)[1])
-}
-
-jfxpeaks<-matrix(0,length(jfindices))
-jfypeaks<-matrix(0,length(jfindices))
-for (i in 1:length(jfindices)){
-    birdcall<-mat[,jfindices[i]]
-    f<-abs(fft(birdcall))
-    jfxpeaks[i]<-sort_index(f)[1]/1000
-    jfypeaks[i]<-(sort_index(f)[2]-sort_index(f)[1])/sort_index(f)[1]
-    xconstraints<-append(xconstraints,min(jfxpeaks))
-    xconstraints<-append(xconstraints,max(jfxpeaks))
-    yconstraints<-append(yconstraints,min(jfypeaks))
-    yconstraints<-append(yconstraints,max(jfypeaks))
-}
+    jfxpeaks<-matrix(0,length(jfindices))
+    jfypeaks<-matrix(0,length(jfindices))
+    for (i in 1:length(jfindices)){
+        birdcall<-mat[,jfindices[i]]
+        f<-abs(fft(birdcall))
+        jfxpeaks[i]<-sort_index(f)[1]/1000
+        jfypeaks[i]<-(sort_index(f)[2]-sort_index(f)[1])/sort_index(f)[1]
+        xconstraints<-append(xconstraints,min(jfxpeaks))
+        xconstraints<-append(xconstraints,max(jfxpeaks))
+        yconstraints<-append(yconstraints,min(jfypeaks))
+        yconstraints<-append(yconstraints,max(jfypeaks))
+    }
 
 #now a test case from the jungle fowl data
     birdcall=mat[,sample(col1:col2,1,replace=F)]
@@ -64,4 +67,14 @@ for (i in 1:length(jfindices)){
         } else {
         decision<-append(decision,0)
     }
-    
+}
+
+if (sum(decision == 1) >= sum(decision == 0)){
+    print("Bird is a Sri Lankan Jungle Fowl")
+}else{
+    print("Bird is not a Sri Lankan Jungle Fowl")
+}
+
+# if at any point you wish to see a scatterplot
+plot(xpeaks,ypeaks)
+points(jfxpeaks,jfypeaks,col='red')
